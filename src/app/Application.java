3d7cbcd2404ae20620579ab2cecfced1a7ef3f17
Paul Friedli <paul.friedli@edufr.ch>
2025-03-21 15:03:51 +0100

    v0.3

---
+++ src/app/Application.java
@@ -0,0 +1,26 @@
+package app;
+
+import models.HotelExcelsior;
+import models.TypeDeVoiture;
+import models.Voiture;
+
+/**
+ * Evaluation sommative N°1 "Hotel-Excelsior".
+ * 
+ * @author <a href="mailto:friedlip@edufr.ch">Paul Friedli</a>
+ * @since 05 février 2025
+ * @version 0.1
+ */
+public class Application {
+
+    /**
+     * La méthode main() de l'application, là où tout commence mais... tout se
+     * termine-t-il bien là ?
+     *
+     * @param args les arguments passés au programme sur la ligne de commande
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
