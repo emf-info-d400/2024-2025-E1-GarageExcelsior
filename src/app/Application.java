d282ec6290b83adf92218a4c62ee3c22a9f6a2c9
Paul Friedli <paul.friedli@edufr.ch>
2025-03-26 10:55:02 +0100

    v0.4

--- src/app/Application.java
+++ src/app/Application.java
@@ -21,6 +21,17 @@ public class Application {
      */
     public static void main(String[] args) {
 
+        //
+        // Ces voitures sont mentionnées dans le diagramme de séquence et vous seront probablement utiles.
+        // Cela vous évitera de faire des fautes de frappe et vous fera gagner du temps.
+        //
+        // Voiture("Bugatti Chiron", "FR-001", TypeDeVoiture.ESSENCE)
+        // Voiture("Tesla Model X", "FR-222222", TypeDeVoiture.ELECTRIQUE)
+        // Voiture("Audi Q7 TDI", "GE-333333", TypeDeVoiture.DIESEL)
+        // Voiture("BMW M3", "FR-99999", TypeDeVoiture.ESSENCE)
+        // Voiture("Skoda Oktavia", "ZH-123456", TypeDeVoiture.ESSENCE)
+        // Voiture("Volvo Polestar", "VS-54321", TypeDeVoiture.ELECTRIQUE)
+        //
     }
 
 }
